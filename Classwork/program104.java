c50a9e1c86320b5ac5f6b2d93736878c59f94c68
Anushka Patare <anupatare07@gmail.com>
2025-11-10 23:27:15 +0530

    Add files via upload

---
+++ Classwork/program104.java
@@ -0,0 +1,46 @@
+import java.util.*;
+//OOP Design
+
+class ArrayX
+{
+    public int Arr[];
+    public int iSize;
+
+    public ArrayX(int no)
+    {
+        System.out.println("Inside constructor..");
+        iSize = no;
+        Arr = new int[iSize];
+    }
+    public void Accept()
+    {
+       Scanner sobj = new Scanner(System.in);
+       int i = 0;
+       System.out.println("Enter the elements of array: ");
+       
+       for(i=0;i<Arr.length;i++)
+       {
+           Arr[i] = sobj.nextInt();
+       } 
+    }
+    public void Display()
+    {
+        int i =0;
+        System.out.println("Elements of the array are: ");
+        for(i=0; i<Arr.length; i++)
+        {
+            System.out.println(Arr[i]);
+        }
+    }
+}
+class program104
+{
+    public static void main(String A[])
+    {
+        ArrayX aobj1 = new ArrayX(5);
+        ArrayX aobj2 = new ArrayX(7);
+        
+        
+
+    }
+}
