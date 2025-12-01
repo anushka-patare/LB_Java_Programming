0a02f3fa464fee05c7905c0b981426e153b09664
Anushka Patare <anupatare07@gmail.com>
2025-12-01 21:55:34 +0530

    Add files via upload

---
+++ Classwork/program245.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class program245
+{
+    public static void main(String A[])
+    {
+        int iCnt = 0;
+        Scanner scanobj = new Scanner(System.in);
+
+        System.out.println("Enter string :");
+        String sobj = scanobj.nextLine();
+        
+        char Arr[] = sobj.toCharArray();
+
+        System.out.println(sobj.length());
+        System.out.println(Arr.length);
+     
+        
+    }
+}
