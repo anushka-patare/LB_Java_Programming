aabafed679dc8c73c3b8d39ff2b8d87cb7787af6
Anushka Patare <anupatare07@gmail.com>
2025-12-10 22:14:33 +0530

    Add files via upload

---
+++ Classwork/program254.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class StringX
+{
+    public String Update(String str)
+    {
+       
+        char Arr[] = str.toCharArray();
+         
+        Arr[0] = '_';
+
+        return new String(Arr);
+    }
+}
+class program254
+{
+    public static void main(String A[])
+    {
+       
+        Scanner scanobj = new Scanner(System.in);
+
+        System.out.println("Enter string :");
+        String sobj = scanobj.nextLine();
+        
+        StringX strobj = new StringX();
+
+        sobj = strobj.Update(sobj);         // CHANGE (NOT PREFERABLE))
+        System.out.println("Updated string is :"+sobj);
+    }
+}
