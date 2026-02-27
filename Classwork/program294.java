cc81a73fdca88b5565ae2a401faff84ef0d7ad73
Anushka Patare <anupatare07@gmail.com>
2026-02-27 22:21:52 +0530

    Add files via upload

---
+++ Classwork/program294.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class program294
+{
+    public static void main(String A[])
+    {
+        int Arr[] = {45,21,90,54,78};
+
+        // This is for each loop
+        
+        for(int no : Arr)
+        {
+            System.out.println(no);
+        }
+
+        int index = Arrays.binarySearch(Arr,90);
+
+        System.out.println("Elements found at : "+index);
+
+    }
+    
+}
